0c467cfb7ecfbd55d7982e112bd0bf64e28a010f
Lauzid <adrianlauzid@gmail.com>
2024-02-01 15:55:37 -0300

    Simple arrow function exercise

--- exercise-1/script.js
+++ exercise-1/script.js
@@ -3,7 +3,7 @@
  * -----------
  * Write a JavaScript program to check two 
  * numbers and return true if one of the number
- * is 100 or if the sum of the two numbers is 100.
+ * is 100 or if the sum of the two numbers is 100
  */
 
 const isEqualTo100 = (a, b) => a === 100 || b === 100 || (a + b === 100);
