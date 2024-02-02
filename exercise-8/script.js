69c7e70f00d90fb7f54cf8dc9e88a5a9e3da1c87
Lauzid <adrianlauzid@gmail.com>
2024-02-02 14:16:51 -0300

    Exercise 8.1

--- exercise-8/script.js
+++ exercise-8/script.js
@@ -5,10 +5,11 @@
  * except their first character.
  */
 
-const str = "";
 
 const concatenateStrings = (str1, str2) =>
-  str.concat(str1.substring(1),(str2.substring(1)));
+  str1.substring(1) + str2.substring(1);
 
 console.log(concatenateStrings('refuse', 'resist'))
+console.log(concatenateStrings('simpler', 'way'))
+console.log(concatenateStrings('abcd', 'efgh'))
 
