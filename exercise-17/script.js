960232abc8e1317687b0491a1a84e0f4c19725f6
Lauzid <adrianlauzid@gmail.com>
2024-02-12 15:19:13 -0300

    Exercise-17.2

--- exercise-17/script.js
+++ exercise-17/script.js
@@ -7,26 +7,35 @@
  * additional properties)
  */
 
-const spell1 = {
-  name: 'Aard',
-  color: 'Yellow',
-  range: 2,
-  active: true
-}
+/**
+ * Definindo objetos:
+ *  Usa-se a mesma sintaxe de uma variável, porém usando const e {} para definir objeto.
+ *  Usando o primeiro exemplo, objA = {a: 1, ...}, onde {(chave):(valor)}.
+ */
+const objA = {a: 1, b: 2, c: 1};
+const objB = {a: 1, b: 1, c: 1};
+const objC = {a: 1, b: 1, d: 1};
 
-const spell2 = {
-  name: 'Aard',
-  color: 'Blue',
-  range: 20,
-  active: false,
-}
+/**
+ * Função recebe dois objetos como parâmetro e compara se o primeiro objeto possui as mesmas
+ * chaves do segundo.
+ * 
+ * Object.keys(a) itera sobre as chaves de um objeto e retorna um Array com os nomes delas
+ * 
+ * .every() é um método de Array Iteration. Esse método checa se todos valores de um array
+ * passam por um teste, se passar, retorna true.
+ * O método every() trabalha sobre o Array retornado por "Object.keys(a)".
+ * 
+ * O "parâmetro" dentro do método every() é uma função de callback que faz o seguinte:
+ *  'key' é a chave atual do objeto 'a' que está sendo verificada.
+ *  'b[key]' verifica se a chave atual 'key' existe no objeto 'b' e retorna o valor associado-
+ *  a essa chave. Se a chave não existir em 'b', 'b[key]' será undefined, o que é avaliado como
+ *  false em JS.
+ */
+const compareObjects = (a, b) => 
+  Object.keys(a).every(key => b[key])
 
-const compareObjects = (obj1, obj2) => {
-  const obj1Keys = Object.keys(obj1).length;
-  const obj2Keys = Object.keys(obj2).length;
-
-  if (obj1Keys < obj2Keys) return false
-  else return true
-};
-
-console.log(compareObjects(spell1, spell2))
+console.log(compareObjects(objA, objB))
+console.log(compareObjects(objB, objB))
+console.log(compareObjects(objB, objC))
+console.log(compareObjects(objA, objC))
